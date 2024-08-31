6cf6e5b4164a59c33a1e6d4220a2148b3759c7da
Jan Veen <jan.veen@posteo.de>
2024-08-31 11:38:52 +0200

    Issue #5 Add sqlite rust project and connect code

---
+++ src/rust/sqlite/src/inject.rs
@@ -0,0 +1,58 @@
+/*  tiny-auth: Tiny OIDC Provider
+ *  Copyright (C) 2019 The tiny-auth developers
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+use crate::error::SqliteError;
+use crate::store::SqliteStore;
+use sqlx::pool::PoolOptions;
+use sqlx::sqlite::SqliteConnectOptions;
+use sqlx::sqlite::SqliteJournalMode::Wal;
+use sqlx::sqlite::SqliteSynchronous::Normal;
+use sqlx::Executor;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::Duration;
+
+pub async fn sqlite_store(url: &str) -> Result<Arc<SqliteStore>, SqliteError> {
+    let options = SqliteConnectOptions::from_str(url)?
+        .read_only(true)
+        .journal_mode(Wal)
+        .busy_timeout(Duration::from_secs(5))
+        .synchronous(Normal)
+        .pragma("cache_size", "1000000000")
+        .foreign_keys(true)
+        .pragma("temp_store", "memory")
+        .analysis_limit(Some(0))
+        .optimize_on_close(true, None);
+    let pool_options = PoolOptions::new()
+        .min_connections(0)
+        .max_lifetime(Some(Duration::from_secs(600)));
+
+    let read_pool_options = pool_options.clone().max_connections(4);
+    let read_pool = read_pool_options.connect_lazy_with(options.clone());
+
+    let write_options = options.read_only(false);
+    let write_pool_options = pool_options.max_connections(1);
+    let write_pool = write_pool_options.connect_lazy_with(write_options);
+
+    write_pool.execute("pragma wal_checkpoint(full)").await?;
+    write_pool.execute("analyze").await?;
+    write_pool.execute("pragma optimize").await?;
+
+    Ok(Arc::new(SqliteStore {
+        read_pool,
+        write_pool,
+    }))
+}
