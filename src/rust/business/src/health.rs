e09632874a8a7dd4e2fbcf3ed1d72c597a8aab2a
Jan Veen <jan.veen@posteo.de>
2024-12-04 19:40:03 +0100

    Issue #105 Add tests

--- src/rust/business/src/health.rs
+++ src/rust/business/src/health.rs
@@ -16,6 +16,8 @@
  */
 
 use async_trait::async_trait;
+#[cfg(test)]
+use mockall::automock;
 use std::sync::Arc;
 use tracing::instrument;
 
@@ -47,11 +49,13 @@ impl HealthCheck {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct HealthStatement {
     pub name: String,
     pub state: bool,
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 pub trait HealthCheckCommand: Send + Sync {
     async fn check(&self) -> bool;
@@ -66,5 +70,37 @@ pub mod inject {
             name: name.to_owned(),
             command,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::health::*;
+    use test_log::test;
+
+    #[test(tokio::test)]
+    async fn all_checks_are_executed() {
+        let mut healthy_command = MockHealthCheckCommand::new();
+        healthy_command.expect_check().times(1).return_const(true);
+        let mut sick_command = MockHealthCheckCommand::new();
+        sick_command.expect_check().times(1).return_const(false);
+        let uut = HealthChecker(vec![
+            inject::health_check("ok", Arc::new(healthy_command)),
+            inject::health_check("nok", Arc::new(sick_command)),
+        ]);
+
+        assert_eq!(
+            vec![
+                HealthStatement {
+                    name: "ok".into(),
+                    state: true,
+                },
+                HealthStatement {
+                    name: "nok".into(),
+                    state: false,
+                }
+            ],
+            uut.execute_all().await
+        );
     }
 }
